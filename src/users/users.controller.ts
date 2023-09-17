d352bd92d3d47487776c3fc68c7eea70ed4baa48
nolood <a9514548799@gmail.com>
2023-09-17 22:36:29 +0500

    feat: complete auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { User } from './users.model';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('self')
+  getSelf(@Req() req: { user: User }) {
+    return this.userService.getUser(req.user.id);
+  }
+}
